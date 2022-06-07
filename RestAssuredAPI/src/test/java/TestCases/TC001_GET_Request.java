4bcc00c89e2ed404f599d42e08a4eb1b624924e1
karuniyya <prasanthi.paluri@devrabbit.com>
2022-06-07 14:34:13 +0530

    min

---
+++ RestAssuredAPI/src/test/java/TestCases/TC001_GET_Request.java
@@ -0,0 +1,30 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TC001_GET_Request {
+
+	@Test
+	 void getweatherDetails()
+	 {
+	  //Specify base URI
+	  RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
+	  
+	  //Request object
+	  RequestSpecification httpRequest=RestAssured.given();
+	  
+	  //Response object
+	  Response response=httpRequest.request(Method.GET,"/Hyderabad");
+	  
+	  //print response in console window
+	  
+	  String responseBody=response.getBody().asString();
+	  System.out.println("Response Body is:" +responseBody);
+		 
+	}
+}
